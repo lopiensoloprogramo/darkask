a8de6d6abe706970107b1993c794be80c9330882
cjean <cjeancito121212@gmail.com>
2025-12-06 18:30:07 -0500

    fix cambio nombre de imagen

--- src/Components/LatestAnsweredQuestions.tsx
+++ src/Components/LatestAnsweredQuestions.tsx
@@ -6,7 +6,7 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import type { Question } from "../types/QuestionsInterfaz";
 import fbIcon from "../assets/fbICONO.png"
 import inIcon from "../assets/inICONO.png";
-import logoBANNER from "../assets/logoBANNER.png"
+import logoBANNER from "../assets/finalbanner.png"
 
 interface UserSummary {
   id: string;
