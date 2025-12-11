986e700653e4020ec6bf93ea31be5adf077d80c3
cjean <cjeancito121212@gmail.com>
2025-12-10 21:31:34 -0500

    cambio de img de banner

--- src/Components/LatestAnsweredQuestions.tsx
+++ src/Components/LatestAnsweredQuestions.tsx
@@ -6,7 +6,7 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import type { Question } from "../types/QuestionsInterfaz";
 import fbIcon from "../assets/fbICONO.png"
 import inIcon from "../assets/inICONO.png";
-import logoBANNER from "../assets/banneractualizado.png"
+import logoBANNER from "../assets/bannersuperactualizado.png"
 
 
 interface UserSummary {
