74dc9ba99e5aa74051127e9acf5dcc1bec685b08
cjean <cjeancito121212@gmail.com>
2025-12-11 05:05:19 -0500

    cambio de img de banner

--- src/Components/LatestAnsweredQuestions.tsx
+++ src/Components/LatestAnsweredQuestions.tsx
@@ -116,8 +116,8 @@ function timeAgo(timestamp: any) {
           src={logoBANNER}
           style={{
             width: "100%",
-            maxWidth: isMobile ? "220px" : "500px",
-            height: isMobile ? "90px" : "100px",
+            maxWidth: isMobile ? "220px" : "700px",
+            height: isMobile ? "90px" : "130px",
             objectFit: "contain",
             animation: "fadeIn .8s ease"
           }}
