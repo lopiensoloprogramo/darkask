54aae442b19904554070f0ef37f1c322c975c475
cjean <cjeancito121212@gmail.com>
2026-04-01 18:36:01 -0500

    debugenado handlelike23

--- src/Components/Profile/ProfileUser.tsx
+++ src/Components/Profile/ProfileUser.tsx
@@ -436,6 +436,18 @@ useEffect(() => {
   return () => unsubscribe();
 }, [authUser]);
 
+useEffect(() => {
+  if (!profileUserId) return;
+
+  const isOwner = authUser?.uid === profileUserId;
+
+  if (isOwner) {
+    setMainTab("feed");
+  } else {
+    setMainTab("profile");
+  }
+
+}, [authUser, profileUserId]);
 
 useEffect(() => {
   if (notifications.length > prevNotifCount) {
