07a07b9635ef2aa9cb126095400b4d1e20c85308
cjean <cjeancito121212@gmail.com>
2026-04-01 19:04:28 -0500

    debugenado handlelike29

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -28,7 +28,7 @@ const [showSearch, setShowSearch] = useState(false);
     padding: isMobile ? "8px 14px" : "10px 28px",
     height: isMobile ? 56 : 64,
 
-   maxWidth: isMobile ? 500 : 1200,
+   maxWidth: isMobile ? 400 : 1200,
     margin: "0 auto"
   };
 
