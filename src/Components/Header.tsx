da7102c35a3cfc01b3ab07f1f72935cfff3b0ea3
cjean <cjeancito121212@gmail.com>
2026-04-05 11:47:28 -0500

    actualización preguntas14

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -65,7 +65,7 @@ const [showSearch, setShowSearch] = useState(false);
         <img
           src={logo}
           style={{
-            width: isMobile ? 140 : 200,
+            width: isMobile ? 150 : 200,
             height: "auto",
             objectFit: "contain",
             display: "block",
