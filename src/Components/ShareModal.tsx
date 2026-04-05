eeced4ef32b184cd55a4ea5994514ec725f910c5
cjean <cjeancito121212@gmail.com>
2026-04-05 07:32:42 -0500

    debug contador diario25

--- src/Components/ShareModal.tsx
+++ src/Components/ShareModal.tsx
@@ -73,8 +73,7 @@ const dataUrl = await toPng(element, {
           <div style={answerContainer}>
             <p style={answerLabel}>Mi respuesta:</p>
            <p style={answerText}>
-          
-                {question.answer?.slice(0, 200)}
+                {question.answer?.slice(0, 250)}
               </p>
                               
                 {question.imageUrl && (
