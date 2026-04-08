080ba815a91d51390663f3d07d72a34a71e7cefd
cjean <cjeancito121212@gmail.com>
2026-04-08 09:32:25 -0500

    Notificaciones de like9

--- src/Components/InternalHeader.tsx
+++ src/Components/InternalHeader.tsx
@@ -28,7 +28,7 @@ const [showSearch, setShowSearch] = useState(false);
     padding: isMobile ? "8px 14px" : "10px 0px",
     height: isMobile ? 56 : 64,
 
-   maxWidth: isMobile ? 400 : 1000,
+   maxWidth: isMobile ? 400 : "100%",
     margin: "0 auto"
   };
 
